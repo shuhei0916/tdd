3033c8d2aa46ffba423161cf30dd2e440ebab265
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-04 23:51:56 +0900

    modified xunit.py, completed chapter20

--- xunit.py
+++ xunit.py
@@ -3,30 +3,50 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
-        method()
-
+        method()    
+        self.tearDown()
+        return result
 
 # メソッドが走ったかどうかをテストするクラス
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+    def testBrokenMethod(self):
+        raise Exception
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run() 
-        assert(self.test.wasRun) # フラグがtrueになったことを確認
-    def testSetUp(self):
+    def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert(test.wasSetUp)
+        assert("setUp testMethod tearDown " == test.log) # フラグがtrueになったことを確認
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
