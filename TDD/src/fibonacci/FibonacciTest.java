7c6a1abf6101e303521aa6f6bec8e67d36f6484a
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-11 10:38:18 +0900

    completed FibonacciTest from page288 of TDD.

---
+++ TDD/src/fibonacci/FibonacciTest.java
@@ -0,0 +1,23 @@
+package fibonacci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FibonacciTest {
+//	実装が関数ひとつだけなため、テストクラスの中に実装コードも書く。
+	int fib(int n) {
+		if (n == 0) return 0;
+		if (n == 2) return 1;
+		return fib(n - 1) + fib(n - 2);
+		
+	}
+	@Test
+	public void testFibonacci() {
+		int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3, 2}};
+		for (int i = 0; i < cases.length; i++)
+			assertEquals(cases[i][1], fib(cases[i][0])); // 第1引数が期待値、第2引数が実測値
+//		assertEquals(1, fib(1));
+//		assertEquals()
+	}
+}
