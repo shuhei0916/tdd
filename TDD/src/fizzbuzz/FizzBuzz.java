424b95c955c19487f3b99804a68446f27ad597ca
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-11 14:19:08 +0900

    completed first part of tddbc.

--- TDD/src/fizzbuzz/FizzBuzz.java
+++ TDD/src/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,12 @@ package fizzbuzz;
 public class FizzBuzz {
 
 	public String convert(int num) {
+		if (num % 3 == 0) {
+			return "Fizz";
+		}
+		if (num % 5 == 0) {
+			return "Buzz";
+		}
 		return String.valueOf(num);
 	}
 	
--- TDD/src/fizzbuzz/FizzBuzzTest.java
+++ TDD/src/fizzbuzz/FizzBuzzTest.java
@@ -2,15 +2,20 @@ package fizzbuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	
+	private FizzBuzz fizzbuzz;
+	@BeforeEach
+	void 前準備() {
+		fizzbuzz = new FizzBuzz();
+	}
+	
 	@Test
 	void _1を渡すと文字列1を返す() throws Exception {
-		// 準備Arrange
-		FizzBuzz fizzbuzz = new FizzBuzz();
-		// 実行Act&検証Assert
 		assertEquals("1", fizzbuzz.convert(1));
 		
 	}
@@ -18,10 +23,17 @@ class FizzBuzzTest {
 	// 1 assertion per test principle
 	@Test
 	void _2を渡すと文字列2を返す() throws Exception {
-		// 準備Arrange
-		FizzBuzz fizzbuzz = new FizzBuzz();
-		// 実行Act&検証Assert
 		assertEquals("2", fizzbuzz.convert(2));
+		
+	}
+	@Test
+	void _3を渡すと文字列Fizzを返す() throws Exception {
+		assertEquals("Fizz", fizzbuzz.convert(3));
+		
+	}
+	@Test
+	void _5を渡すと文字列Buzzを返す() throws Exception {
+		assertEquals("Buzz", fizzbuzz.convert(5));
 		
 	}
 
