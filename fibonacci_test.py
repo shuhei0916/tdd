27b695422729612d8607e994bfe49a2b63daa51a
shuhei0916 <shuheiito.stc@gmail.com>
2024-05-24 00:27:17 +0900

    completed fibonacci.py

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -2,9 +2,12 @@ import unittest
 from fibonacci import fib
 
 class TestFibonacci(unittest.TestCase):
-    # 0を返す
-    def test_zero(self):
-        self.assertEqual(0, fib(0))
+    # fib(0)は0を返す
+    def test_fib(self):
+        for n, expected in ((0, 0), (1, 1), (2, 1), (3, 2)):
+            with self.subTest(n=n):
+                self.assertEqual(fib(n), expected) # expectedが第2引数（Juinitと逆！）
+    
     
 if __name__ == '__main__':
     unittest.main()
